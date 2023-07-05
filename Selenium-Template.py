e5af61470831e9ece02442b87c52d29fdb1c0d71
mori3803 <hasanix9@gmail.com>
2023-07-05 18:02:37 +0330

    Update Selenium-Template.py

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -2,9 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 import chromedriver_autoinstaller
+from selenium.webdriver.common.by import By
+import os
+import time
 from pyvirtualdisplay import Display
 display = Display(visible=0, size=(800, 800))  
 display.start()
+
+username = "7065836" #os.environ["username"]
+password = "123456" #os.environ["password"]
 
 chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
                                       # and if it doesn't exist, download it automatically,
@@ -15,7 +21,8 @@ chrome_options = webdriver.ChromeOptions()
 options = [
   # Define window size here
    "--window-size=1200,1200",
-    "--ignore-certificate-errors"
+    "--ignore-certificate-errors",
+    "--add_experimental_option"
  
     #"--headless",
     #"--disable-gpu",
@@ -35,6 +42,25 @@ driver = webdriver.Chrome(options = chrome_options)
 
 driver.get('https://food.esfahansteel.ir/Lego.Web/Kevlar/Account/Login')
 print(driver.title)
-with open('./GitHub_Action_Results.txt', 'w') as f:
-    f.write(f"This was written with a GitHub action {driver.title}")
+driver.find_element("id", "UserName").send_keys(username)
+driver.find_element("id", "Password").send_keys(password)
+driver.find_element("id", "btnSubmit").click()
+time.sleep(15)
 
+f3 = driver.find_elements(By.CLASS_NAME,'favmenu-item-inner-wrapper')
+f3[1].click()
+#f.send_keys(Keys.RETURN)
+time.sleep(15)
+foods = driver.find_elements(By.CLASS_NAME,'food-name')
+for food in foods : 
+    print(food.text)
+
+
+#text = driver.find_element("class", "text-title")
+#driver.find_element(By.XPATH , '//a | //*[contains(concat( " ", @class, " " ), concat( " ", "title", " " ))]').click()
+#time.sleep(10)
+txt = driver.find_element(By.CLASS_NAME, "text-title")
+with open('./GitHub_Action_Results.txt', 'w') as f:
+    f.write(f"This was written with a GitHub action {txt.text}")
+driver.quit()
+print(txt.text)
