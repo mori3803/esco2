0bd45c7918aeeb8e3a10ddd1711d5f4b6a067d15
mori3803 <hasanix9@gmail.com>
2024-09-15 22:10:22 +0330

    Update Selenium-Template.py

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -23,6 +23,7 @@ options.add_argument("--window-size=1200,1200")
 options.add_argument("--ignore-certificate-errors")
 options.add_argument("--enable-logging")
 options.add_argument('--disable-blink-features=AutomationControlled')
+options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
 options.set_capability('goog:loggingPrefs', {'performance': 'ALL'})
 
 driver = webdriver.Chrome(options=options)
