098bafce8fa8d26f34ce71b7107d839254c3199a
mori3803 <hasanix9@gmail.com>
2024-09-18 11:02:19 +0330

    Update Selenium-Template.py

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -9,7 +9,7 @@ import time
 import datetime
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from pyvirtualdisplay import Display
-
+"""
 display = Display(visible=0, size=(800, 800))  
 display.start()
 
@@ -51,6 +51,7 @@ driver.get('https://food.esfahansteel.ir/Lego.Web/Kevlar/Account/Login?ReturnUrl
 print(driver.title)
 with open('./GitHub_Action_Results.txt', 'w') as f:
     f.write(f"This was written with a GitHub action {driver.title}")
+"""
 """
 display = Display(visible=0, size=(800, 800))  
 display.start()
@@ -158,5 +159,40 @@ with open('./GitHub_Action_Results.txt', 'w') as f:
     for food in foods:
         f.write(f"{(food.text).replace('آب معدني نيم ليتري', '')}\n")
 """
+display = Display(visible=0, size=(800, 800))  
+display.start()
+
+chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
+                                      # and if it doesn't exist, download it automatically,
+                                      # then add chromedriver to path
+
+chrome_options = webdriver.ChromeOptions()    
+# Add your options as needed    
+options = [
+  # Define window size here
+   "--window-size=1200,1200",
+    "--ignore-certificate-errors"
+ 
+    #"--headless",
+    #"--disable-gpu",
+    #"--window-size=1920,1200",
+    #"--ignore-certificate-errors",
+    #"--disable-extensions",
+    #"--no-sandbox",
+    #"--disable-dev-shm-usage",
+    #'--remote-debugging-port=9222'
+]
+
+for option in options:
+    chrome_options.add_argument(option)
+PROXY = "80.249.112.162:80"
+chrome_options.add_argument('--proxy-server=%s' % PROXY)
+    
+driver = webdriver.Chrome(options = chrome_options)
+
+driver.get('http://github.com')
+print(driver.title)
+with open('./GitHub_Action_Results.txt', 'w') as f:
+    f.write(f"This was written with a GitHub action {driver.title}")
 driver.quit()
 #print(txt.text)
